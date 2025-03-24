76ff9361c7171c167839afa111516df2c473d024
henry.lai <henry.lai@synapxe.sg>
2025-03-24 11:24:06 +0800

    other config files edit

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/test/setup.ts',
+    coverage: {
+        reporter: ['text', 'json', 'html'],
+      },
   },
 })
