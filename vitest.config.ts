eb22b732c88764ce3e5bc8a50bfc47b0ab659b7a
henry.lai <henry.lai@synapxe.sg>
2025-03-24 16:48:34 +0800

    setup package files and add test case

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    setupFiles: './src/test/setup.ts',
     coverage: {
-        reporter: ['text', 'json', 'html'],
-      },
+      reporter: ["text", "json", "html"],
+    },
+    // setupFiles: ["./vitest.setup.ts"],
+    // deps: {
+    //   inline: [
+    //     "@testing-library/react-hooks",
+    //     // Add other problematic packages as needed
+    //   ],
+    // },
+
+    // // Add testTransformMode here
+    // testTransformMode: {
+    //   web: ["**/*.{js,jsx,ts,tsx}"],
+    // },
+    // // Optional: exclude directories you don't want to test
+    // exclude: ["**/node_modules/**", "**/dist/**", "**/.{git,cache}/**"],
   },
-})
+});
